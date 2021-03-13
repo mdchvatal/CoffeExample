f2a0ef3856d9e1da76261d34c2d128e09744949e
Matt Chvatal <mdchvatal@gmail.com>
2021-03-13 09:17:02 -0500

    Committed

--- src/CoffeeTester.java
+++ src/CoffeeTester.java
@@ -9,7 +9,6 @@ class CoffeeTester {
 		OldCoffeeMachine old = new OldCoffeeMachine();
 		CoffeeTouchscreenAdapter newCM = new CoffeeTouchscreenAdapter(old);
 		assertEquals(); 
-		//??Testing a bunch of void methods? I don't understand this and none of the instructors will look at the assignments. Please forward any demerits to them.
 	}
 
 }
